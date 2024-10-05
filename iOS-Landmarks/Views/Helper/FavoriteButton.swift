39786167653494df89f36e879121b5891bc1b75e
Raúl Arroyo <ral4576@gmail.com>
2024-10-05 14:36:31 +0200

    Chapter 3 - Section 6

---
+++ iOS-Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  iOS-Landmarks
+//
+//  Created by Raúl Arroyo Lubián on 05/10/2024.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
