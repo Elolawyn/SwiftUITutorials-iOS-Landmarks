f19c121b56033afd776176e6b7aa8d483951befa
Raúl Arroyo <ral4576@gmail.com>
2024-10-05 11:02:50 +0200

    Initial Commit

---
+++ iOS-Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-Landmarks
+//
+//  Created by Raúl Arroyo Lubián on 05/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Landmarks/iOS_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_LandmarksApp.swift
+//  iOS-Landmarks
+//
+//  Created by Raúl Arroyo Lubián on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
